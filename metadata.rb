bc59da30dd7384629eab6c9fe824f5e56efa3bf8
Josh Goodall <inopinatus@inopinatus.org>
2019-09-02 12:23:14 +1000

    Bump metadata and remove Berksfile lock from repo

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,13 @@ name 'chef-upgrade'
 maintainer 'Josh Goodall'
 maintainer_email 'inopinatus@inopinatus.org'
 license 'MIT'
-description 'Wraps and manages a Chef upgrade'
-long_description 'Wraps and manages a Chef upgrade'
-version '0.0.1'
+description 'Wraps and stickies a Chef upgrade'
+long_description 'Wraps and stickies a Chef upgrade'
+version '1.0.0'
+source_url 'https://github.com/inopinatus/chef-upgrade'
+issues_url 'https://github.com/inopinatus/chef-upgrade/issues'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+
+supports 'ubuntu', '>= 18.04'
 
 depends 'chef_client_updater'

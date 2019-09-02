8739dfb0ea5e5ad329efc287cba186829a6b7e12
Josh Goodall <inopinatus@inopinatus.org>
2019-09-02 12:16:25 +1000

    Better comment.

--- recipes/cannot_self_terminate.rb
+++ recipes/cannot_self_terminate.rb
@@ -3,6 +3,7 @@
 #
 # Copyright 2019, Josh Goodall
 
-# Do not allow the Chef updater to override our preference for exec
+# The Chef updater will try to kill its own process. This causes setup failure.
+# We force it to accept our "exec" configuration by monkey-patching the LWRP.
 update_provider = Chef.provider_handler_map.get(node, :chef_client_updater)
 update_provider.prepend(CannotSelfTerminate)
